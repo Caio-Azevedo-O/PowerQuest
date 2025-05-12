0d2048dfa5f5d29dc9127751393cc99c8627f5e6
GustasSilva17 <159274296+GustasSilva17@users.noreply.github.com>
2025-05-12 15:44:47 -0300

    Update planos.php

--- generico/planos.php
+++ generico/planos.php
@@ -1,1 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Planos- Power Quest</title>
+    <link rel="stylesheet" href="css/planos.css">
+    <link rel="icon" type="image/png" href="imagem/Logo.png">
+</head>
+<body>
+    <div class="header">
+    <div class="logo">
+      <img src="imagem/Power.png" alt="Logo" class="logo-img">
+    </div>
+    <div class="nav-links">
+      <a href="index.php" class="nav-link">Início</a>
+      <a href="planos.php" class="nav-link">Planos</a>
+      <a href="login.php" class="nav-link">Login</a>
+    </div>
+    </div>
+
+    <div class="container">
+    <div class="card light">
+      <h2>Power⚡Light</h2>
+      <p>Esse plano é o mais básico que temos<br>para vc que não quer se comprometer em treinar</p>
+      <h3>A partir de<br><strong>R$MENOS DINHEIRO</strong><br>12 meses de plano</h3>
+      <button>Assinar Plano</button>
+      <ul>
+        <li>Acesso Limitado a +17.500 Academias</li>
+        <li>Leve 5 Amigos por mês para treinar com você</li>
+        <li>Cadeira de Massagem</li>
+        <li>Área de musculação e Aeróbicos</li>
+      </ul>
+    </div>
+
+    <div class="card max">
+      <h2>Power⚡Max</h2>
+      <p>Treine na maioria das academias do Brasil<br>e algumas na América Latina em qualquer dia da semana</p>
+      <h3>A partir de<br><strong>R$DINHEIRO</strong><br>12 meses de plano</h3>
+      <button>Assinar Plano</button>
+      <ul>
+        <li>Acesso Limitado a +17.500 Academias</li>
+        <li>Leve 5 Amigos por mês para treinar com você</li>
+        <li>Cadeira de Massagem</li>
+        <li>Área de musculação e Aeróbicos</li>
+      </ul>
+    </div>
+
+    <div class="card ultra">
+      <h2>Ultra⚡Power</h2>
+      <p>Treine em qualquer academia que temos<br>parceria que seja no Brasil ou na América Latina<br><small>São +17.500 unidades em 15 países</small></p>
+      <h3>A partir de<br><strong>R$MAIS DINHEIRO</strong><br>12 meses de plano</h3>
+      <button>Assinar Plano</button>
+      <ul>
+        <li>Acesso Limitado a +17.500 Academias</li>
+        <li>Leve 5 Amigos por mês para treinar com você</li>
+        <li>Cadeira de Massagem</li>
+        <li>Área de musculação e Aeróbicos</li>
+      </ul>
+    </div>
+    </div>
+
+
+
+    <footer class="footer">
+    <div class="footer-container">      
+      <img class="footer-logo" src="imagem/Power.png" alt="Logo">
+      
+      <div class="footer-links">
+        <a href="#" class="footer-link">Início</a>
+        <a href="#" class="footer-link">Planos</a>
+        <a href="#" class="footer-link">Consulta</a>
+        <a href="#" class="footer-link">Contato</a>
+      </div>
+    </div>
+
+    <div class="footer-info">
+      <div class="footer-left">
+        <h3>Quem Somos??</h3>
+        <p>
+          Bem-vindo a Power Quest, o seu espaço dedicado à saúde, bem-estar e superação! Somos mais que um site – somos uma comunidade apaixonada por transformar vidas, proporcionando a cada aluno a motivação, o apoio e os recursos necessários para alcançar seus objetivos fitness.
+          Nossa missão é simples: inspirar e capacitar você a ser a melhor versão de si mesmo. Com profissionais qualificados, equipamentos de última geração e uma vasta variedade de programas de treinamento, oferecemos o ambiente perfeito para quem busca uma vida mais saudável e equilibrada.
+        </p>
+      </div>
+      <div class="footer-right">
+        <div class="social-media">
+          <a href="#"><img src="https://img.icons8.com/ios-filled/25/ffffff/instagram-new.png" alt="Instagram" /></a>
+          <a href="#"><img src="https://img.icons8.com/ios-filled/25/ffffff/whatsapp.png" alt="WhatsApp" /></a>
+          <a href="#"><img src="https://img.icons8.com/ios-filled/25/ffffff/facebook-new.png" alt="Facebook" /></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
 
